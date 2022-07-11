627f539f3d43cae3d49367daa6505f4d51f6c1b6
KW <kenneth_sl_wong@msn.com>
2022-07-11 19:53:17 -0400

    added button click handlers to change colors when pressed, added isWall property to node

--- src/components/PathfinderMain/index.js
+++ src/components/PathfinderMain/index.js
@@ -3,7 +3,35 @@ import GridNode from "../GridNode";
 
 function PathfinderMain() {
 
+    //Grid state
     const [state, setState] = useState([]);
+
+    //Button states with variables for their class names
+    const [startSelected,setStart] = useState(false);
+    const [finishSelected,setFinish] = useState(false);
+    const [wallsSelected,setWalls] = useState(false);
+
+    const classNameBase = "col-3 btn btn-secondary mx-5";
+    const classNameClicked = "col-3 btn btn-info mx-5";
+
+    //Click handlers for setting start, finish and walls
+    const startHandler = () => {
+        setStart(!startSelected);
+        setWalls(false);
+        setFinish(false);
+    }
+
+    const finishHandler = () => {
+        setStart(false);
+        setWalls(false);
+        setFinish(!finishSelected);
+    }
+
+    const wallsHandler = () => {
+        setStart(false);
+        setWalls(!wallsSelected);
+        setFinish(false);
+    }
 
     //Set initial start and end nodes
     let startRow = 5;
@@ -23,7 +51,8 @@ function PathfinderMain() {
             row,
             col,
             isStart: (row === startRow && col === startCol),
-            isFinish: (row === finishRow && col === finishCol)
+            isFinish: (row === finishRow && col === finishCol),
+            isWall: false
         }
     };
 
@@ -40,31 +69,42 @@ function PathfinderMain() {
         setState(grid);
     }
 
+    //Reset grid function
+    const resetGrid = () => {
+        startRow = 5;
+        startCol = 5;
+        finishRow = 15;
+        finishCol = 35;
+
+        makeGrid();
+    }
+
     return (
         <div className="w-100 justify-content-center">
             <h1 className="text-center my-3">Pathfinder</h1>
             <div className="row main-button-container w-100 pb-4 justify-content-center">
                 <button className="col-4 btn btn-success mx-5">Go!</button>
-                <button className="col-2 btn btn-danger mx-5">Reset Grid</button>
+                <button className="col-2 btn btn-danger mx-5" onClick={resetGrid}>Reset Grid</button>
             </div>
             <div className="row toolbar w-100 pb-4 justify-content-center">
-                <button className="col-3 btn btn-info mx-5">Set Start</button>
-                <button className="col-3 btn btn-info mx-5">Set End</button>
-                <button className="col-3 btn btn-info mx-5">Toggle Walls</button>
+                <button className={`${startSelected && classNameClicked} ${!startSelected && classNameBase}`} onClick={startHandler}>Set Start</button>
+                <button className={`${finishSelected && classNameClicked} ${!finishSelected && classNameBase}`} onClick={finishHandler}>Set Finish</button>
+                <button className={`${wallsSelected && classNameClicked} ${!wallsSelected && classNameBase}`} onClick={wallsHandler}>Toggle Walls</button>
             </div>
             <div className="grid-container py-2 w-75 align-items-end justify-content-center d-flex flex-column">
                 {state.map((row, i) => {
                     return (
                         <div key={i} className="row d-flex">
                             {row.map((node, j) => {
-                                const { row, col, isStart, isFinish } = node;
+                                const { row, col, isStart, isFinish, isWall } = node;
                                 return (
                                     <GridNode
                                         key={j}
                                         col={col}
                                         row={row}
                                         isStart={isStart}
-                                        isFinish={isFinish}>
+                                        isFinish={isFinish}
+                                        isWall = {isWall}>
                                     </GridNode>
                                 );
                             })}
